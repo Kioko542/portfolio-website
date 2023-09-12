24570cdce063c810a39eb38cb658e7355b2ca498
Emmanue <kioxmanu7@gmail.com>
2023-09-13 00:05:17 +0300

    work done

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // all javascript here
+document.addEventListener("DOMContentLoaded", function () {
+    const menuIcon = document.querySelector(".menu-icon");
+    const navLinks = document.querySelector(".nav-links");
+
+    menuIcon.addEventListener("click", function () {
+        navLinks.classList.toggle("active");
+    });
+});

43eb43a884662ccc87fa865841f2dbbe0d69dbfb
Emmanue <kioxmanu7@gmail.com>
2023-09-13 17:05:13 +0300

    work  done

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
 // all javascript here
+const htmlProgress = 90;
+const cssProgress = 80;
+const jsProgress = 75;
+const reactProgress = 70;
+
+document.getElementById("html-progress").style.width = htmlProgress + "%";
+document.getElementById("css-progress").style.width = cssProgress + "%";
+document.getElementById("js-progress").style.width = jsProgress + "%";
+document.getElementById("react-progress").style.width = reactProgress + "%";
+
+
+
 document.addEventListener("DOMContentLoaded", function () {
     const menuIcon = document.querySelector(".menu-icon");
     const navLinks = document.querySelector(".nav-links");
